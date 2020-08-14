25b42f3b6cb8079fe65f57777a3074c405be049e
Panupat-Chanate <panupat.c@psru.ac.th>
2020-08-14 11:03:08 +0700

    pong game

--- app/src/main/java/com/example/project_pong/Ball.java
+++ app/src/main/java/com/example/project_pong/Ball.java
@@ -1,4 +1,67 @@
 package com.example.project_pong;
 
+import android.graphics.RectF;
+
 public class Ball {
+    private RectF mRect;
+    private float mXVelocity;
+    private float mYVelocity;
+    private float mBallWidth;
+    private float mBallHeight;
+
+    Ball(int screenX){
+        // Make the ball square and 1% of screen width of the screen width
+        //หาร 100 คือขนาดของบอล
+        mBallWidth = screenX / 20;
+        mBallHeight = screenX / 20;
+        // Initialize the RectF with 0, 0, 0, 0
+        mRect = new RectF();
+    }
+    RectF getRect(){
+        return mRect;
+    }
+
+    void update(long fps){
+        mRect.left = mRect.left + (mXVelocity / fps);
+        mRect.top = mRect.top + (mYVelocity / fps);
+
+        mRect.right = mRect.left + mBallWidth;
+        mRect.bottom = mRect.top + mBallHeight;
+    }
+
+    void reverseYVelocity(){
+        mYVelocity = -mYVelocity;
+    }
+    void reverseXVelocity(){
+        mXVelocity = -mXVelocity;
+    }
+
+    void reset(int x, int y){
+        mRect.left = x / 2;
+        mRect.top = 0;
+        mRect.right = x / 2 + mBallWidth;
+        mRect.bottom = mBallHeight;
+
+        mYVelocity = -(y / 3);
+        mXVelocity = (y / 3);
+    }
+
+    void increaseVelocity(){
+        // increase the speed by 10%
+        mXVelocity = mXVelocity * 1.1f;
+        mYVelocity = mYVelocity * 1.1f;
+
+    }
+
+    void batBounce(RectF batPosition){
+        float batCenter = batPosition.left + (batPosition.width() / 2);
+        float ballCenter = mRect.left + (mBallWidth / 2);
+        float relativeIntersect = (batCenter - ballCenter);
+        if(relativeIntersect < 0){
+            mXVelocity = Math.abs(mXVelocity);
+        }else{
+            mXVelocity = -Math.abs(mXVelocity);
+        }
+        reverseYVelocity();
+    }
 }
